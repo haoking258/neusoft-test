ee1209cf05a2061d6d23d485ceb19c9853b3160e
UnderSakura <149467677@qq.com>
2023-07-25 20:28:39 +0800

    添加了赞颂Dennis的Java

---
+++ FirstRequest.java
@@ -0,0 +1,5 @@
+public class FirstRequest {
+    public static void main(String[] args) {
+        System.out.println("Dennis is the best teacher ! And MediaLab is the best Lab in the whole world!");
+    }
+}
